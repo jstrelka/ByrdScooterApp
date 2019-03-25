cdb4be37d3b3af8824531d61073f5645aaffa5ba
Justin Strelka <jstrelka@student.cccs.edu>
2019-03-25 10:27:03 -0600

    changed headers for bird files

---
+++ BinTree.h
@@ -0,0 +1,64 @@
+/*
+Program Info
+Name: JUSTIN STRELKA
+Creation Date: 11/12/2018
+Program Name: BINTREE.H (BIRDAPP)
+Description: BINTREE CLASS DECLARATION (CONTAINER CLASS)
+Version: 11/18/2018
+Sources: C++ DATA STRUCTURES/ CPLUSPLUS.COM
+*/
+
+// MACRO GUARD BEGIN
+#ifndef BINTREE_H
+#define BINTREE_H
+
+// INCLUDE LIBRARIES
+#include<iostream>
+#include<string>
+#include "BinNode.h"
+
+// NAMESPACE
+namespace strelka_bird_1
+{
+	// DECLARE CLASS TEMPLATE
+	template <class Type>
+	// BEGIN BINTREE CLASS DECLARATION
+	class BinTree
+	{
+	public:
+		// DEFAULT CONSTRUCTOR
+		BinTree(); //(EMPTY TREE
+		BinTree(const BinTree<Type> & source); // COPY CONSTRUCTOR
+		BinTree<Type>& operator=(const BinTree<Type> & source); // ASSIGNMENT OPERATOR
+		~BinTree(); // DESTRUCTOR (EMPTY TREE)
+		bool add(Type dataIn); // BINTREE ADD FUNCTION
+		bool remove(Type& dataIn); // BINTREE REMOVE FUNCTION
+		int countItems() { return treeCount; } // RETURN HOW MANY ITEMS ARE IN BINTREE
+		// OTHER MEMBER FUNCS - DATA PERSISTING
+		bool readFile(std::string fileName);
+		bool writeFile(std::string fileName,
+			char delimiter = '\n');
+		void printAll(); // PRINT ENTIRE BINTREE
+		void inOrderTraverse(void process(Type&, int)); // TRAVERSE BINTREE IN ORDER
+		BinNode<Type>* getRoot() { return root; } // OBTAIN ROOT* OF BINTREE
+
+	private:
+		void copyTree(const BinNode<Type>* source); // RECURSIVELY COPY CONTENTS OF TREE
+		void writeFile(BinNode<Type>* cursor, std::ofstream& outFile, char delimiter); // WRITE TREE DATA TO FILE
+		BinNode<Type>* allocBinNode(Type dataIn); // ALLOCATE NEW BINNODE POINTER
+		void freeAllBinNodes(BinNode<Type>* cursor); // RECURSIVELY FREE ENTIRE BINTREE
+		void inOrderTraverse(BinNode<Type>* rootPtr, int& counter, void process(Type&, int)); // RECURSIVELY TRAVERSE BINTREE
+		BinNode<Type>* remove(BinNode<Type>*& root, Type& dataIn); // REMOVE ITEM FROM BINTREE USING RECURSION
+		BinNode<Type>* minValueNode(BinNode<Type>*& node); // OBTAIN MIN VALUE NODE 
+		int treeCount = 0; // COUNTER TO KEEP TRACK OF TREE NODES
+		BinNode<Type> * root; // ROOT POINTER
+	};
+
+	// STANDALONE FUNC TO PRINT CONTENTS OF TREE (USED WITH TRAVERSE TREE IN PRINTALL PUBLIC FUNCTION)
+	template<class Type>
+	void print(Type & cursor, int counter);
+}
+
+#include "BinTree.tem"
+
+#endif
---
+++ Field.cpp
@@ -0,0 +1,116 @@
+/*
+Program Info
+Name: JUSTIN STRELKA
+Creation Date: 10/1/2018
+Program Name: FIELD.CPP (BIRDAPP)
+Description: FIELD CLASS DEFINITION
+Version: 10/20/2018
+Sources: C++ DATA STRUCTURES/ CPLUSPLUS.COM
+*/
+
+// INCLUDE DIRECTIVES
+#include "Field.h"
+
+// NAMESPACE
+namespace strelka_bird_1
+{
+	Field::Field() // default  constructor
+		: std::string()  // calls the string default constructor
+	{
+		arraySize = 0;
+	}
+	Field::Field(std::string& str)
+		: std::string(str) // calls the string copy constructor
+	{
+		arraySize = 0;
+	}
+	Field::Field(const char* cStr)
+		: std::string(cStr)  // calls the constructor passes const char*
+	{
+		arraySize = 0;
+	}
+
+	// field split function will parse a string passed into seperate array fields on commas (or delimiter passed).
+	std::string * Field::split(char delimiter)
+	{
+		int commaIndex = 0;
+		arraySize = 1;
+		std::string* tmpPtr = NULL;
+		std::string tmp = *this;
+		std::string* fields = new std::string[arraySize]; // creates array with 1 spot because of array size the * is a wild card
+		int used = 0; // used is currently 0 because array is initialized but empty
+
+		commaIndex = this->find(delimiter); // looks at the contents of the address pointed to.
+
+		do
+		{
+			if (used >= arraySize) // if full - no more room DO THIS!!!
+			{ // memory allocation at runtime
+				arraySize++;
+				tmpPtr = new std::string[arraySize];
+				for (int i = 0; i < used; i++)
+					tmpPtr[i] = fields[i];
+				//copy (fields, fields + used, tmpPtr),
+				delete[] fields;
+				fields = tmpPtr;
+			}
+			fields[used++] = tmp.substr(0, commaIndex);
+			tmp = tmp.erase(0, commaIndex + 1);
+			commaIndex = tmp.find(delimiter);
+		} while (commaIndex != std::string::npos);
+		return fields;
+	}
+
+	// function will capitalize all of string passed to it. 
+	std::string Field::strToUpper(std::string& inputString)
+	{
+		for (int i = 0; i < inputString.length(); i++)
+		{
+			if (inputString[i] <= 'z' && inputString[i] >= 'a')
+			{
+				inputString[i] -= 32;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		return inputString;
+	}
+
+	// function will cast to lower case all of string passed to it.
+	std::string Field::strToLower(std::string & inputString)
+	{
+		for (int i = 0; i < inputString.length(); i++)
+		{
+			if (inputString[i] <= 'Z' && inputString[i] >= 'A')
+			{
+				inputString[i] += 32;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		return inputString;
+	}
+
+	// istream >> operator for field
+	std::istream& operator>>(std::istream& is, Field & fld)
+	{
+		// get everything up untill the enter key is pressed
+		if (is.peek() == '\n') // check to make sure a newline (enter key)
+			is.ignore();       // from previous cin >>..; was not left in the stream
+		getline(is, fld, '\n');  // can use getline because field is a string
+		return is;
+	}
+
+	// ifstream operator for field
+	std::ifstream& operator>>(std::ifstream& ifs, Field & fld)
+	{
+		if (ifs.peek() == '\n')
+			ifs.ignore();
+		getline(ifs, fld, ',');
+		return ifs;
+	}
+}
